3905e8c33503bffc742b8afed3869d1bc15fec3b
Vikkadin <104149770+Vikkadin@users.noreply.github.com>
2022-10-17 13:31:22 +0300

    feat: install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Dictionary from './components/Dictionary';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React Dictionary Project</h1>
+      <Dictionary />
     </div>
   );
 }
