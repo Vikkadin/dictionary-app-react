4aa9358d11063e31f3d7324261a39320e2bfe195
Vikkadin <104149770+Vikkadin@users.noreply.github.com>
2022-10-18 22:55:30 +0300

    feat: add Photos component

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,21 +1,34 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import Results from './Results';
+import Photos from './Photos';
 import '../css/Dictionary.css'
 
 function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [response, setResponse] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResponse(response.data[0]);
+    }
+
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search(event) {
         event.preventDefault();
-
         const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+        axios.get(apiUrl).catch(function (error) {
+            alert(`Sorry, I don't know word "${keyword}". Try another one :)`);
+        });
+
+        const pexelsApiKey = "563492ad6f917000010000010f12befde61a4f03997db0b07a1b52fe";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        let headers = { "Authorization": `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleKeywordChange(event) {
@@ -35,6 +48,7 @@ function Dictionary() {
             </section>
             <section>
                 <Results result={response} />
+                <Photos photos={photos} />
             </section>
         </div>
     )
